815d695ac8495f0f99281f666bbb4dc80859f8e0
Susan Tabassum <sttabassum93@gmail.com>
2020-08-06 20:49:26 -0400

    Stuck on delivering number + type of fruit

--- fruit_basket.py
+++ fruit_basket.py
@@ -20,14 +20,48 @@ with open(filename) as csvfile:
 
 #print("Fields: " + str(headers))
 
+# print total number of fruit in basket
+print("There are " + str(len(rows)) + " fruits total in this basket.")
+
 # get all the fruits & print
 fruits = []
 for each_row in rows:
     fruits.append(each_row[0])
 
-# print total number of fruit in basket
-print("Number of fruit total: " + str(len(fruits)))
-
 # get types of fruit & print
 uniquefruits = set(fruits)
-print("There are " + str(len(uniquefruits)) + " types of fruit: " + str(uniquefruits))
+print("There are " + str(len(uniquefruits)) + " types of fruit: " + ", ".join(uniquefruits) + ".")
+
+# empty fruit lists
+# TODO: extract fruit names from data
+apples = []
+oranges = []
+grapefruit = []
+pineapple = []
+watermelon = []
+
+# get individual lists of fruits
+index = 0
+
+for row in fruits:
+    if fruits[index] == 'apple':
+        apples.append(fruits[index])
+    elif fruits[index] == 'orange':
+        oranges.append(fruits[index])
+    elif fruits[index] == 'grapefruit':
+        grapefruit.append(fruits[index])
+    elif fruits[index] == 'pineapple':
+        pineapple.append(fruits[index])
+    elif fruits[index] == 'watermelon':
+        watermelon.append(fruits[index])
+    index += 1
+
+# get number of fruit by type & print
+# TODO: order by descending
+numapple = str(len(apples))
+numorange = str(len(oranges))
+numgrapefruit = str(len(grapefruit))
+numpineapple = str(len(pineapple))
+numwatermelon = str(len(watermelon))
+
+print("In this basket there are " + numapple + " apples, " + numorange + " oranges, " + numgrapefruit + " grapefruit, " + numpineapple + " pineapple, " + numwatermelon + " watermelon.")
